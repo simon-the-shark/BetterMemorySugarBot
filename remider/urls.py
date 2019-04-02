91a10bbed61d50243a5a855386b85be0004e929e
simon <simon_rekin@o2.pl>
2019-04-02 20:44:51 +0200

    debug=False, updated template

--- remider/urls.py
+++ remider/urls.py
@@ -1,13 +1,9 @@
 from django.urls import path
-from django.conf.urls import url
-from django.views.generic import RedirectView
 
 from .views import get_from_api, calculate, notify, home
-
 urlpatterns = [
     path("", home),
     path("get/", get_from_api, name="get"),
     path("calculate/", calculate, name="calculate"),
     path("notify/", notify, name="notify"),
-    # url(r'^favicon\.ico$', RedirectView.as_view(url='/static/images/favicon.ico')),
 ]
