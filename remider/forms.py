c0337ffdff5bd8f4b2f3376cae255d957c434e0c
simon <simon_rekin@o2.pl>
2019-04-08 21:58:32 +0200

    added secret form in api_secret view

--- remider/forms.py
+++ remider/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class GetSecretForm(forms.Form):
-    apisecret = forms.CharField(required=True)
+    apisecret = forms.CharField(required=True, widget=forms.PasswordInput)
