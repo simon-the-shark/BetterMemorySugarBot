73fc4720f4174abc82eef178ec922032c19014f7
simon <simon_rekin@o2.pl>
2019-04-17 13:40:30 +0200

    changed url to regex url

--- remider/urls.py
+++ remider/urls.py
@@ -1,16 +1,16 @@
-from django.urls import path
+from django.urls import re_path
 from django.views.generic import TemplateView
 
 from .views import reminder_view, file, auth, menu, upload, ManagePhoneNumbersView
 from .decorators import secret_key_required
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="remider/home.html"), name="home"),
-    path("reminder/", reminder_view, name="reminder"),
-    path("ATriggerVerify.txt", file),
-    path("auth/", auth, name='get_secret'),
-    path("menu/", menu, name='menu'),
-    path("upload/", upload, name="upload"),
-    path("phonenumbers/", secret_key_required(ManagePhoneNumbersView.as_view()), name="manage_ph_numbers"),
+    re_path(r"^$", TemplateView.as_view(template_name="remider/home.html"), name="home"),
+    re_path(r"^reminder/$", reminder_view, name="reminder"),
+    re_path(r"^ATriggerVerify.txt$", file),
+    re_path(r"^auth/$", auth, name='get_secret'),
+    re_path(r"^menu/$", menu, name='menu'),
+    re_path(r"^upload/$", upload, name="upload"),
+    re_path(r"^phonenumbers/$", secret_key_required(ManagePhoneNumbersView.as_view()), name="manage_ph_numbers"),
 
 ]
