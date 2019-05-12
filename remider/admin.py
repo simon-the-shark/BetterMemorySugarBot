8d9c45c07f2849d06664eb7fb148e6550a01ae32
simon <simon_rekin@o2.pl>
2019-05-12 16:17:08 +0200

    registered LastTriggerSet

--- remider/admin.py
+++ remider/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import InfusionChanged, SensorChanged
+
+from .models import InfusionChanged, SensorChanged, LastTriggerSet
 
 admin.site.register(InfusionChanged)
 admin.site.register(SensorChanged)
+admin.site.register(LastTriggerSet)
