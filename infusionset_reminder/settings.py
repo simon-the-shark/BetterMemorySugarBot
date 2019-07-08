cd8e01dc6f9182eef62b46104bf92af49177794c
simon <simon_rekin@o2.pl>
2019-07-08 23:39:24 +0200

    marking translations part 1

--- infusionset_reminder/settings.py
+++ infusionset_reminder/settings.py
@@ -14,6 +14,7 @@ import os
 
 import django_heroku
 from decouple import config
+from django.utils.translation import ugettext_lazy as _
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -46,6 +47,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -104,8 +106,12 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
+LANGUAGES = (
+    ("pl", _("Polish")),
+    ("en", _("English"))
+)
 
-LANGUAGE_CODE = config("LANGUAGE_CODE")
+LANGUAGE_CODE = config("LANGUAGE_CODE", default="en")
 
 TIME_ZONE = "UTC"
 
@@ -115,6 +121,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, "locale"),
+)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
