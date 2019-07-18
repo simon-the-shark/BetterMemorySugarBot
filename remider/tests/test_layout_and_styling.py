14b9e861884b69eb3b6ec0390d7f968addd782c4
simon-the-shark <simon_rekin@o2.pl>
2019-07-18 15:14:48 +0200

    added testing of loading css in menu view

--- remider/tests/test_layout_and_styling.py
+++ remider/tests/test_layout_and_styling.py
@@ -1,5 +1,6 @@
 from .base import FunctionalTest, check_internet_connection
 from django.shortcuts import reverse
+from django.test import override_settings
 from unittest import skipIf
 
 
@@ -24,5 +25,17 @@ class CSSTests(FunctionalTest):
         self.assertAlmostEqual(
             form.location['x'],
             41,
+            delta=20
+        )
+
+    @skipIf(not check_internet_connection(), "no internet connection")
+    @override_settings(SECRET_KEY="mycoolsecretkey")
+    def test_menu_view(self):
+        self.browser.get(self.live_server_url + reverse("menu")+"?key=mycoolsecretkey")
+        self.browser.set_window_size(1024, 768)
+        settings = self.browser.find_element_by_css_selector(".display-4")
+        self.assertAlmostEqual(
+            settings.location['x'],
+            32,
             delta=20
         )
