dfa905f7590f99d8a05184112b4b099a57039a92
simon-the-shark <simon_rekin@o2.pl>
2019-07-22 18:51:49 +0200

    added file testing after cleanup

--- remider/tests/test_functional_uploading.py
+++ remider/tests/test_functional_uploading.py
@@ -7,6 +7,20 @@ import os.path
 
 
 class UploadingTest(FunctionalTest):
+    def setUp(self):
+        super().setUp()
+        try:
+            with open(os.path.join(settings.STATIC_ROOT, "uplouded", "ATriggerVerify.txt")) as file:
+                self.file = file.read()
+        except FileNotFoundError:
+            self.file = False
+
+    def tearDown(self):
+        super().tearDown()
+        if self.file:
+            with open(os.path.join(settings.STATIC_ROOT, "uplouded", "ATriggerVerify.txt"), "w+") as file:
+                file.write(str(self.file))
+
     @override_settings(DEBUG=True, SECRET_KEY="mycoolsecretkey")
     def test_templates(self):
         response = self.client.get(reverse("upload") + "?key=mycoolsecretkey")
@@ -30,5 +44,4 @@ class UploadingTest(FunctionalTest):
     @override_settings(SECRET_KEY="mycoolsecretkey", LANGUAGE_CODE='en', app_name="benc-test", DEBUG=True)
     def test_uploading(self):
         self.uplouding_test(file_path=os.path.join(settings.BASE_DIR, "remider", "tests", "ATriggerVerify.txt"))
-        print("bemc")
         self.uplouding_test(file_path=os.path.join(settings.BASE_DIR, "remider", "tests", "ATriggerVerify2.txt"))
