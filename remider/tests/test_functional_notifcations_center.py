6f7d982cd2ec502d4edb9c2da1e60e897dcf40fe
simon <simon_rekin@o2.pl>
2019-08-09 19:56:05 +0200

    fixed test

--- remider/tests/test_functional_notifcations_center.py
+++ remider/tests/test_functional_notifcations_center.py
@@ -1,6 +1,7 @@
-from .base import FunctionalTest
 from django.shortcuts import reverse
 from django.test import override_settings
+
+from .base import FunctionalTest
 
 
 class NotificationCentrumTest(FunctionalTest):
@@ -19,14 +20,14 @@ class NotificationCentrumTest(FunctionalTest):
         self.assertTrue(sms_checkbox.is_selected())
         ifttt_checkbox.click()
         self.browser.find_elements_by_css_selector(".btn-primary")[0].click()
-        self.wait_for_finding(lambda: self.check_alert())
+
         ifttt_checkbox = self.wait_for_finding(lambda: self.browser.find_element_by_id("id_ifttt_notifications"))
         sms_checkbox = self.wait_for_finding(lambda: self.browser.find_element_by_id("id_sms_notifications"))
         self.assertTrue(ifttt_checkbox.is_selected())
         self.assertTrue(sms_checkbox.is_selected())
         sms_checkbox.click()
         self.browser.find_elements_by_css_selector(".btn-primary")[0].click()
-        self.check_alert()
+
         ifttt_checkbox = self.wait_for_finding(lambda: self.browser.find_element_by_id("id_ifttt_notifications"))
         sms_checkbox = self.wait_for_finding(lambda: self.browser.find_element_by_id("id_sms_notifications"))
         self.assertTrue(ifttt_checkbox.is_selected())
